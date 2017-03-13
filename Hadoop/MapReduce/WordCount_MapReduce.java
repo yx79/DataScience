bf8c4db76d770ee2fc9c28745af76ce59879bb67
Aiden Xie <yx79@georgetown.edu>
2017-03-12 23:11:32 -0400

    Update WordCount_MapReduce.java

--- Hadoop/MapReduce/WordCount_MapReduce.java
+++ Hadoop/MapReduce/WordCount_MapReduce.java
@@ -1,3 +1,33 @@
+/*
+
+Using map reduce to count word frequency.
+
+https://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html#Example%3A+WordCount+v1.0
+
+Have you met this question in a real interview? Yes
+Example
+chunk1: "Google Bye GoodBye Hadoop code"
+chunk2: "lintcode code Bye"
+
+
+Get MapReduce result:
+    Bye: 2
+    GoodBye: 1
+    Google: 1
+    Hadoop: 1
+    code: 2
+    lintcode: 1
+
+testcase:
+chunk1: "Google Bye GoodBye Hadoop code"
+chunk2: "lintcode code Bye"
+
+*/
+
+
+
+
+
 /**
  * Definition of OutputCollector:
  * class OutputCollector<K, V> {
